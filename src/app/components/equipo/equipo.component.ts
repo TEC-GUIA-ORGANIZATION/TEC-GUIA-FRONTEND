74097e918aa7c8a213a69838f1133dfbba20f497
Johanx22x <johanrodsa2210@gmail.com>
2024-05-15 14:11:52 -0600

    Create filtering options

--- src/app/components/equipo/equipo.component.ts
+++ src/app/components/equipo/equipo.component.ts
@@ -5,6 +5,7 @@ import { ProfesorGuia } from '../../models/profesor-guia.model';
 import { Sede } from '../../models/sede.model';
 import { GestorAutenticacion } from '../../services/gestor-autenticacion.service';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
@@ -14,12 +15,20 @@ import { NgxPaginationModule } from 'ngx-pagination';
   imports: [
     CommonModule,
     NavbarComponent,
-    NgxPaginationModule
+    NgxPaginationModule,
+    FormsModule
   ]
 })
 export class EquipoComponent {
   pageSize: number = 5; // Number of items per page
   p: number = 1; // Current page, initialized to 1
+  sedes: string[] = Object.values(Sede); // To store the list of sedes
+  selectedSede: string = ''; // To store selected sede filter value
+  selectedCoordinador: string = ''; // To store selected coordinador filter value
+  profesorSeleccionado: ProfesorGuia | null = null;
+  editMode: boolean = false;
+  originalProfesores: ProfesorGuia[] = []; // To store the original list of profesores
+
   profesores: ProfesorGuia[] = [
     new ProfesorGuia(
       '6643c6211bcce33b1cc6056f',
@@ -204,7 +213,7 @@ export class EquipoComponent {
       'Profesor 2',
       'Apellido 1',
       'Apellido 2',
-      Sede.CARTAGO,
+      Sede.SAN_JOSE,
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/220px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg',
       'Profesor Guía',
       '1',
@@ -220,7 +229,7 @@ export class EquipoComponent {
       'Profesor 2',
       'Apellido 1',
       'Apellido 2',
-      Sede.CARTAGO,
+      Sede.SAN_CARLOS,
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/220px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg',
       'Profesor Guía',
       '1',
@@ -236,7 +245,7 @@ export class EquipoComponent {
       'Profesor 2',
       'Apellido 1',
       'Apellido 2',
-      Sede.CARTAGO,
+      Sede.SAN_CARLOS,
       'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/220px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg',
       'Profesor Guía',
       '1',
@@ -246,10 +255,24 @@ export class EquipoComponent {
       true
     ),
   ];
-  profesorSeleccionado: ProfesorGuia | null = null;
-  editMode: boolean = false;
 
-  constructor(private gestorAutenticacion: GestorAutenticacion) {}
+  constructor(private gestorAutenticacion: GestorAutenticacion) {
+    this.originalProfesores = [...this.profesores];
+  }
+
+  filterProfesores() {
+    // Filter profesores based on selectedSede and selectedCoordinador
+    this.profesores = this.originalProfesores.filter(profesor => {
+      let sedeFilter = !this.selectedSede || profesor.sede === this.selectedSede;
+      let coordinadorFilter = !this.selectedCoordinador || profesor.esCoordinador.toString() === this.selectedCoordinador;
+      return sedeFilter && coordinadorFilter;
+    });
+  }
+
+  // Add this method to handle changes in the selectedSede and selectedCoordinador filters
+  onFilterChange() {
+    this.filterProfesores(); // Call the filterProfesores function to update the list
+  }
 
   seleccionarProfesor(profesor: ProfesorGuia): void {
     this.profesorSeleccionado = profesor;
@@ -260,6 +283,18 @@ export class EquipoComponent {
 
     if (user !== null) {
       if (user.rol === 'admin') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  hasMainPrivileges(): boolean {
+    var user = this.gestorAutenticacion.getCurrentUser();
+
+    if (user !== null) {
+      if (user.rol === 'admin' && user.sede === Sede.CARTAGO) {
         return true;
       }
     }
